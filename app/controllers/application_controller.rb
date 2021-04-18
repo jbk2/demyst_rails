54d71773de0f50b997215f838d09622a2d71a377
jbk <james@bibble.com>
2021-04-18 22:05:39 +0100

    creating comments table and showing them in show_post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,8 @@ class ApplicationController < ActionController::Base
 
 	def show_post
  		post = Post.find(params['id'])
- 		render 'application/show_post', locals: { post: post }
+ 		comments = connection.execute('SELECT * FROM comments WHERE comments.post_id = ?', params['id'])
+ 		render 'application/show_post', locals: { post: post, comments: comments }
 	end
 
 	def edit_post
